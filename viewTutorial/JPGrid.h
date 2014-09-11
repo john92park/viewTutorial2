e5523f9127c6026d8bc96fd239e6ec7127e91205
Jun Hong Park <JP@wl-215-37.wpa.claremont.edu>
2014-09-11 10:48:31 -0700

    viewTutorial (second half) completed

---
+++ viewTutorial/JPGrid.h
@@ -0,0 +1,13 @@
+//
+//  JPGrid.h
+//  viewTutorial
+//
+//  Created by Jun Hong Park on 9/11/14.
+//  Copyright (c) 2014 John Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPGrid : UIView
+
+@end
---
+++ viewTutorial/JPGrid.m
@@ -0,0 +1,31 @@
+//
+//  JPGrid.m
+//  viewTutorial
+//
+//  Created by Jun Hong Park on 9/11/14.
+//  Copyright (c) 2014 John Park. All rights reserved.
+//
+
+#import "JPGrid.h"
+
+@implementation JPGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
